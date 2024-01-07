80353fb3f60daed0e90c00bff3548073a7445c9b
rejuancse <rejuancse@gmail.com>
2024-01-07 19:57:03 +0600

    Album query script updated

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
 import { usersReducer } from './slices/usersSlice';
+import { albumsApi } from './apis/albumsApi';
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer
   },
 });
 
